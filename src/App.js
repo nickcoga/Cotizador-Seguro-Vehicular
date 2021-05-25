8b753a0362b7bfa7dd8ba86baaa54c0987117618
Nick Correa <nickcorreagarcia@gmail.com>
2021-05-25 16:39:09 -0500

    Renaming files for structure order

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import DatosAuto from "./screens/Datos_Auto/Datos_Auto/DatosAuto";
-import Home from "./screens/home/home/Home";
+import CarData from "./screens/car_data/CarData";
+import Home from "./screens/home/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/datosauto/:id" component={DatosAuto} />
+        <Route path="/datosauto/:id" component={CarData} />
         <Route />
       </Switch>
     </Router>
