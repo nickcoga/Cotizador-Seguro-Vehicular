80e3d136f3e26e1c33e54f0ff506aaed712ae7a1
Nick Correa <nickcorreagarcia@gmail.com>
2021-05-24 21:54:35 -0500

    Setup rounting App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import DatosAuto from "./screens/Datos_Auto/Datos_Auto/DatosAuto";
+import Home from "./screens/home/home/Home";
 
 function App() {
-  return <Fragment> App Succesfull</Fragment>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/datosauto/:id" component={DatosAuto} />
+        <Route />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
