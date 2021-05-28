4a60073bc1905ac73b069ac847dafb17568c1398
Nick Correa <nickcorreagarcia@gmail.com>
2021-05-28 02:52:48 -0500

    Add props in App.js component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CarData from "./screens/car_data/CarData";
 import Home from "./screens/home/Home";
 
 function App() {
+  const [user, setUser] = useState();
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/datosauto/:id" component={CarData} />
+        <Route
+          exact
+          path="/"
+          render={(props) => <Home {...props} setUser={setUser} />}
+        />
+        <Route
+          path="/cardata/:id"
+          render={(props) => <CarData {...props} user={user} />}
+        />
         <Route />
       </Switch>
     </Router>
