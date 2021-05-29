9c6731ab4cd46d92acdac6eb5d07e305b4599921
Nick Correa <nickcorreagarcia@gmail.com>
2021-05-29 04:24:04 -0500

    Create CarData screen

--- src/screens/car_data/CarData.js
+++ src/screens/car_data/CarData.js
@@ -1,15 +1,20 @@
-import React, { Fragment, useState } from "react";
-// import { useParams } from "react-router-dom";
+import React, { Fragment } from "react";
 import Header from "../../shared/Header";
 import FormCarData from "./components/FormCarData";
-import Stepper from "./components/Stepper";
+import Stepper from "../../shared/Stepper.js";
 
-export default function CarData({ user }) {
+export default function CarData({ user, year, setYear, brand, setBrand }) {
   return (
     <Fragment>
       <Header showBorder={true} />
       <Stepper />
-      <FormCarData user={user} />
+      <FormCarData
+        user={user}
+        year={year}
+        setYear={setYear}
+        brand={brand}
+        setBrand={setBrand}
+      />
     </Fragment>
   );
 }
