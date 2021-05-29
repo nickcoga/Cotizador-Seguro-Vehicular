5949af7c2aeba723d9257ef03a78fb28f075fcb2
Nick Correa <nickcorreagarcia@gmail.com>
2021-05-29 04:33:11 -0500

    Add routes and props in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BuildPlan from "./screens/build_plan/BuildPlan";
 import CarData from "./screens/car_data/CarData";
 import Home from "./screens/home/Home";
 
 function App() {
   const [user, setUser] = useState();
+  const [plate, setPlate] = useState();
+  const [year, setYear] = useState();
+  const [brand, setBrand] = useState();
 
   return (
     <Router>
@@ -12,13 +16,35 @@ function App() {
         <Route
           exact
           path="/"
-          render={(props) => <Home {...props} setUser={setUser} />}
+          render={(props) => (
+            <Home
+              {...props}
+              setUser={setUser}
+              plate={plate}
+              setPlate={setPlate}
+            />
+          )}
         />
         <Route
           path="/cardata/:id"
-          render={(props) => <CarData {...props} user={user} />}
+          render={(props) => (
+            <CarData
+              {...props}
+              user={user}
+              year={year}
+              setYear={setYear}
+              brand={brand}
+              setBrand={setBrand}
+            />
+          )}
         />
-        <Route />
+
+        <Route
+          path="/buildplan"
+          render={(props) => (
+            <BuildPlan {...props} plate={plate} year={year} brand={brand} />
+          )}
+        />
       </Switch>
     </Router>
   );
