3502789ab10d8ca92baf91e753d9248be489659a
Nick Correa <nickcorreagarcia@gmail.com>
2021-05-30 04:49:56 -0500

    Add Components in screen BuildPlan

--- src/screens/build_plan/BuildPlan.js
+++ src/screens/build_plan/BuildPlan.js
@@ -1,16 +1,20 @@
 import React, { Fragment } from "react";
+import ButtonReturn from "../../shared/ButtonReturn";
 import Header from "../../shared/Header";
 import Stepper from "../../shared/Stepper";
 import Coverage from "./components/Coverage";
 import PlanCoverage from "./components/PlanCoverage";
+import ContentPrice from "./components/ContentPrice";
 
 export default function BuildPlan({ plate, year, brand }) {
   return (
     <Fragment>
       <Header />
       <Stepper />
+      <ButtonReturn />
       <Coverage plate={plate} year={year} brand={brand} />
       <PlanCoverage />
+      <ContentPrice />
     </Fragment>
   );
 }
