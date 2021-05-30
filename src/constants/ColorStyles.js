c74f2dbaa55798c98b7c15b8bc7ccd99b93162f4
Nick Correa <nickcorreagarcia@gmail.com>
2021-05-30 04:56:37 -0500

    Create ColorStyles constant

---
+++ src/constants/ColorStyles.js
@@ -0,0 +1,61 @@
+export const colors = {
+  LightPurple: "#F7F8FC",
+  Purple: "#6769FF",
+  DarkPurple: "#494f66",
+  BorderGray: "#E4E8F7",
+  BorderlightGray: "#f0f2fa",
+  LightGray: "#A3ABCC",
+  Gray: "#676F8F;",
+  DarkGray: "#494F66",
+  Pink: "#F48FB1",
+  LightPink: "#FFC1E3",
+  DarkPink: "#BF5F82",
+  White: "#FFFFFF",
+  Red: "#EF3340",
+  Green: "#7CC954",
+};
+
+// const users = [
+//   {
+//     id: 1,
+//     name: "Jhon Doe",
+//     dni: 45789632,
+//     phone: "951357486",
+//   },
+//   {
+//     id: 2,
+//     name: "Juan Perez",
+//     dni: 48759632,
+//     phone: "987456123",
+//   },
+//   {
+//     id: 3,
+//     name: "Nico",
+//     dni: 46312598,
+//     phone: "954685231",
+//   },
+// ];
+
+// const cars = [
+//   {
+//     userId: 1,
+//     id: 1,
+//     placa: "QWE1234",
+//     year: 2019,
+//     marca: "Wolkswagen",
+//   },
+//   {
+//     userId: 2,
+//     id: 2,
+//     placa: "ASD1234",
+//     year: 2018,
+//     marca: "Toyota",
+//   },
+//   {
+//     userId: 3,
+//     id: 3,
+//     placa: "ZXC1234",
+//     year: 2020,
+//     marca: "Chevrolet",
+//   },
+// ];
