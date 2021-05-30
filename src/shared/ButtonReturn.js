4bbf2c888e838904f7e9964c226882ad957f6d94
Nick Correa <nickcorreagarcia@gmail.com>
2021-05-30 04:46:46 -0500

    Create Shared ButtonReturn component

---
+++ src/shared/ButtonReturn.js
@@ -0,0 +1,57 @@
+import styled from "@emotion/styled";
+import ChrevronLeft from "../assets/chevron_left_orange.svg";
+import { colors } from "../constants/ColorStyles";
+
+export default function ButtonReturn() {
+  return (
+    <Content>
+      <img className="circle" src={ChrevronLeft} alt="Return Icon" />
+      <div>Volver</div>
+    </Content>
+  );
+}
+
+const Content = styled.div`
+  display: none;
+  position: absolute;
+  top: 11vh;
+  left: 15vw;
+  align-items: center;
+  gap: 4px;
+  text-transform: uppercase;
+  .circle {
+    height: 24px;
+    width: 24px;
+    padding: 2px;
+    border: 1px solid ${colors.Red};
+    color: ${colors.Red};
+    border-radius: 80px;
+    text-align: center;
+  }
+  @media (min-width: 768px) {
+    position: absolute;
+    top: 10vh;
+    left: 37vw;
+    width: 8vw;
+    gap: 4px;
+    display: initial;
+    display: flex;
+    align-items: center;
+    font-family: Lato;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 12px;
+    line-height: 16px;
+    text-transform: uppercase;
+    color: ${colors.LightGray};
+    .circle {
+      height: 24px;
+      width: 24px;
+      padding: 2px;
+      border: 1px solid ${colors.Red};
+      color: ${colors.Red};
+      border-radius: 80px;
+      text-align: center;
+    }
+  }
+`;
