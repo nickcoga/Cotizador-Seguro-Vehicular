3a7e5efbc008de75711125dab1d682cc697d203c
Nick Correa <nickcorreagarcia@gmail.com>
2021-06-01 00:41:58 -0500

    Create Api Fetch file

---
+++ src/services/api_fetch.js
@@ -0,0 +1,16 @@
+const BASE_URL =
+  "https://my-json-server.typicode.com/nickcoga/Cotizador-Seguro-Vehicular/";
+
+async function apiFetch(...args) {
+  const response = await fetch(...args);
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.errors);
+  }
+  if (response.status !== 204) {
+    return await response.json();
+  }
+  return true;
+}
+
+export { BASE_URL, apiFetch };
